e5633778c489d4990c89c4a832a99f07a943c7ea
jaelll-lll <martinezvillavicenciog086@gmail.com>
2026-08-12 14:38:04 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-if not game:IsLoaded() then game.Loaded:Wait() end loadstring(request({Url = "https://raw.githubusercontent.com/jaelll-lll/Doggue-Hub/main/Sources/Blade%20Ball.lua"}).Body)()
+loadstring(request({Url = "https://raw.githubusercontent.com/jaelll-lll/Doggue-Hub/main/Sources/Blade%20Ball.lua"}).Body)()
